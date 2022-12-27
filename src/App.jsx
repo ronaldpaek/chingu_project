1958a9167d75b4c1dcbe9f208947dd6c68c90264
Ronald Paek <ronaldpaek@gmail.com>
2022-12-27 13:58:36 -0800

    set up getBooks function with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Book from './components/Book'
 
@@ -5,12 +6,35 @@ const App = () => {
     // console.log(JSON.stringify(import.meta.env))
     const [books, setBooks] = useState([])
     const [search, setSearch] = useState('')
+
+    const getBooks = async (search) => {
+        const baseURL = 'https://www.googleapis.com/books/v1/volumes?q='
+        const query = search
+        const index = 0
+        const url = `${baseURL}${query}&index=${index}`
+
+        try {
+            const response = await axios.get(url)
+            console.log(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
   return (
     <div className='app container'>
         <h1 className='title'>Book finder</h1>
         <div className='search'>
-            <input className='search__input' type="text" placeholder="Search for a book" />
-            <button className='btn'>Search</button>
+            <input
+                className='search__input'
+                type="text"
+                placeholder="Search for a book"
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+                className='btn'
+                onClick={() => getBooks(search)}
+            >Search</button>
         </div>
         <div className='books'>
             {books.map((book, index) => (
