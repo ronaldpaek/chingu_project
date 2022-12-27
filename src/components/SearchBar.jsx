4adb72f4766ce3c299b5c46257004cf5508e2eb7
Ronald Paek <ronaldpaek@gmail.com>
2022-12-26 21:20:35 -0800

    added search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const SearchBar = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=40")
+            .then(response => response.json())
+            .then(data => console.log(data))
+    });
+
+  return (
+    <div className='search'>
+        <input className='search__input' type="text" placeholder="Search for a book" />
+        <button className='btn'>Search</button>
+    </div>
+  )
+}
+
+export default SearchBar;
+
+/**
+ * book cover
+ * book title
+ * author
+ * publisher
+ * see book id
+ */
